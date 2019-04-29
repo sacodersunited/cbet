d3d700b03669b13137288e063c32dc2669ec7d5a
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-04-29 13:19:15 -0500

    adds react-icons and adds them to the top navbar

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,75 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
-import { Container, Navbar, Nav } from "react-bootstrap"
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap"
+import {
+  FaFacebookSquare,
+  FaTwitterSquare,
+  FaLinkedin,
+  FaUserLock,
+} from "react-icons/fa"
 
 const Header = ({ siteTitle }) => (
   <>
-    <Navbar bg="dark" variant="dark">
+    <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
         <Link className="navbar-brand" to="/">
           {siteTitle}
         </Link>
-        <Nav className="mr-auto">
-          <Link to="/about/" className="nav-link" activeClassName="active">
-            About
-          </Link>
-          <Link to="/programs/" className="nav-link" activeClassName="active">
-            Programs
-          </Link>
-          <Link to="/schedule/" className="nav-link" activeClassName="active">
-            Schedule
-          </Link>
-          <Link to="/admissions/" className="nav-link" activeClassName="active">
-            Admissions
-          </Link>
-          <Link to="/financial-aid/" className="nav-link" activeClassName="active">
-            Financial Aid
-          </Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Link to="/about/" className="nav-link" activeClassName="active">
+              About
+            </Link>
+            <Link to="/programs/" className="nav-link" activeClassName="active">
+              Programs
+            </Link>
+            <Link to="/schedule/" className="nav-link" activeClassName="active">
+              Schedule
+            </Link>
+            <Link
+              to="/admissions/"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Admissions
+            </Link>
+            <Link
+              to="/financial-aid/"
+              className="nav-link"
+              activeClassName="active"
+            >
+              Financial Aid
+            </Link>
+            <NavDropdown title="Programs" id="basic-nav-dropdown">
+              <NavDropdown.Item href="/programs/">BMET Cert</NavDropdown.Item>
+              <NavDropdown.Item href="/programs/">BMET Degree</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="/programs/">
+                IT / Networking
+              </NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Nav className="social-nav">
+            <a href="https://facebook.com" target="_blank" className="nav-link">
+              <FaFacebookSquare />
+            </a>
+            <a href="https://twitter.com" target="_blank" className="nav-link">
+              <FaTwitterSquare />
+            </a>
+            <a
+              href="https://www.linkedin.com/"
+              target="_blank"
+              className="nav-link"
+            >
+              <FaLinkedin />
+            </a>
+            <Link to="/" className="nav-link">
+              <FaUserLock />
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   </>
