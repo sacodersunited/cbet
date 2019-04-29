addfba6f224f0b01ca83a7b6b99e74c583bcf163
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-04-29 11:57:12 -0500

    adds container to the page level

--- src/components/layout.js
+++ src/components/layout.js
@@ -31,21 +31,13 @@ const Layout = ({ children }) => (
           crossOrigin="anonymous"
         />
         <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
-            {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
+
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
       </>
     )}
   />
