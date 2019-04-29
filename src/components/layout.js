3e1866f684c03102c5e26cbb8b19650ab3ed53ff
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-04-28 22:31:25 -0500

    adds react bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,6 @@ import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
-import "./layout.css"
 
 const Layout = ({ children }) => (
   <StaticQuery
@@ -25,6 +24,12 @@ const Layout = ({ children }) => (
     `}
     render={data => (
       <>
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+          integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+          crossorigin="anonymous"
+        />
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
