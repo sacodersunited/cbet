cc85879da3f86db2ba144d5f05fb7502cd07bc4c
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-05-06 21:59:13 -0500

    adds content to footer from One Drive.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Container, Row, Col } from "react-bootstrap"
 import styled from "styled-components"
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa"
 
 const FooterSection = styled.footer`
   color: whitesmoke;
@@ -8,32 +9,90 @@ const FooterSection = styled.footer`
   padding: 96px 0;
 `
 
+// TODO: Add links as we build these pages
+
 const Footer = () => (
   <FooterSection className="bg-dark">
     <Container>
       <Row>
-        <Col md={4}>
+        <Col>
           <h2 style={{ textTransform: "uppercase" }}>CBET </h2>
           <p>@CBET 2019</p>
         </Col>
         <Col>
-          <p>About Us</p>
-          <p>Contact</p>
-          <p>Terms and Conditions</p>
+          <a className="d-block text-light mb-3" href="#">
+            Academic Calendar
+          </a>
+          <a className="d-block text-light mb-3" href="#">
+            Employment
+          </a>
+          <a className="d-block text-light mb-3" href="#">
+            Student Catalog
+          </a>
         </Col>
         <Col>
-          <p>Facebook</p>
-          <p>Twitter</p>
-          <p>Instagram</p>
+          <a className="d-block text-light mb-3" href="#">
+            Provide Website Feedback
+          </a>
+          <a className="d-block text-light mb-3" href="#">
+            Accessibility Statement
+          </a>
+          <a className="d-block text-light mb-3" href="#">
+            Privacy Statement
+          </a>
+        </Col>
+        <Col>
+          <a
+            target="_blank"
+            className="d-block text-light mb-3"
+            href="https://www.facebook.com/CareerInstituteofTechnology/"
+          >
+            <FaFacebookF
+              className="mr-3"
+              style={{ verticalAlign: "text-bottom" }}
+            />
+            Facebook
+          </a>
+          {/* TODO: Add twitter link */}
+          <a
+            target="_blank"
+            className="d-block text-light mb-3"
+            href="https://twitter.com"
+          >
+            <FaTwitter
+              className="mr-3"
+              style={{ verticalAlign: "text-bottom" }}
+            />
+            Twitter
+          </a>
+          {/* TODO: Add linkedIn link */}
+          <a
+            target="_blank"
+            className="d-block text-light mb-3"
+            href="https://linkedin.com"
+          >
+            <FaLinkedinIn
+              className="mr-3"
+              style={{ verticalAlign: "text-bottom" }}
+            />
+            LinkedIn
+          </a>
         </Col>
         <Col>
           <p>
-            11550 IH-10 West, Suite 190 <br />
+            11550 IH-10 West <br />
+            Suite 190 <br />
             San Antonio, Texas 78230
           </p>
-          <p>(210) 233-1102</p>
-          <p>(866) 866-9027</p>
-          <p>admissiondept@cittx.edu</p>
+          <a className="d-block text-light" href="tel:210-233-1102">
+            (210)233-1102
+          </a>
+          <a className="d-block text-light" href="tel:866-866-9027">
+            (866) 866-9027
+          </a>
+          <a className="d-block text-light" href="mailto:webmaster@cittx.edu">
+            webmaster@cittx.edu
+          </a>
         </Col>
       </Row>
     </Container>
