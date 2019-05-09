568fbdfa88049e901595de08b14570a7a014b043
Paul Castillo <azrael7@gmail.com>
2019-05-08 19:25:18 -0500

    Added animations to BMET degree page.

--- src/pages/programs.js
+++ src/pages/programs.js
@@ -6,6 +6,8 @@ import GraphicHeader from "../components/graphicHeader"
 import headerImg from "../images/bmet-bg.jpg"
 import { Container, Row, Col, Button, Image } from "react-bootstrap"
 import styled from "styled-components"
+import Fade from "react-reveal/Fade"
+import Slide from "react-reveal/Slide"
 import BMETImg from "../images/bmet-tech.jpg"
 import BMETImg1 from "../images/bmetDegree/bmet-1.jpg"
 import BMETImg2 from "../images/bmetDegree/bmet-2.jpg"
@@ -16,7 +18,6 @@ const ProgramDetail = styled.div`
   padding: 46px 0 46px 40px;
   color: white;
 `
-
 const Programs = () => (
   <LayoutDetail>
     <SEO title="Programs" />
@@ -24,22 +25,24 @@ const Programs = () => (
     <Container className="mb-5">
       <Row>
         <Col md={7}>
-          <h1 className="display-5 text-uppercase">
-            BioMedical Equipment Technician
-          </h1>
-          <p>
-            The Associate of Applied Science (AAS) Degree in Biomedical
-            Equipment Technology-IDL is an interactive distance learning
-            (online) program that provides a realistic and up-to-date overview
-            of the ever-changing career of Healthcare Technology Management. The
-            program is designed to prepare the student to seek a variety of
-            entry-level positions in the field, as well as advancing in a
-            current BMET career.
-          </p>
+          <Fade left>
+            <h1 className="display-5 text-uppercase">
+              BioMedical Equipment Technician
+            </h1>
+            <p>
+              The Associate of Applied Science (AAS) Degree in Biomedical
+              Equipment Technology-IDL is an interactive distance learning
+              (online) program that provides a realistic and up-to-date overview
+              of the ever-changing career of Healthcare Technology Management.
+              The program is designed to prepare the student to seek a variety
+              of entry-level positions in the field, as well as advancing in a
+              current BMET career.
+            </p>
 
-          <Link to="/">
-            <Button className="text-uppercase"> Apply Today</Button>
-          </Link>
+            <Link to="/">
+              <Button className="text-uppercase"> Apply Today</Button>
+            </Link>
+          </Fade>
         </Col>
       </Row>
     </Container>
@@ -47,14 +50,17 @@ const Programs = () => (
       <Col md={6}>
         <ProgramDetail className="mb-5">
           <h2 className="text-uppercase mb-5">Program Overview</h2>
-          <h4>Program Length</h4>
-          <p>15 Months</p>
-          <h4>Program Composition</h4>
-          <p>Online</p>
-          <h4>Classes</h4>
-          <p>Classes 5 days a week(Monday - Friday)</p>
-          <p>Class Length: 3 hours</p>
-          <p>Class Times: 8am - 12am, 12pm-3pm, 4pm - 5pm</p>
+          <Fade>
+            <h4>Program Length</h4>
+
+            <p>15 Months</p>
+            <h4>Program Composition</h4>
+            <p>Online</p>
+            <h4>Classes</h4>
+            <p>Classes 5 days a week(Monday - Friday)</p>
+            <p>Class Length: 3 hours</p>
+            <p>Class Times: 8am - 12am, 12pm-3pm, 4pm - 5pm</p>
+          </Fade>
         </ProgramDetail>
       </Col>
       <Col md={6}>
@@ -72,14 +78,16 @@ const Programs = () => (
       <h2 className="text-uppercase mb-5">Curriculum</h2>
       <Row className="mb-5">
         <Col md={6}>
-          <h4>General Education</h4>
-          <ul>
-            <li>Business Professional Communication</li>
-            <li>American History</li>
-            <li>Everyday Mathematics</li>
-            <li>English Composition I</li>
-            <li>Introduction to Computers</li>
-          </ul>
+          <Slide left>
+            <h4>General Education</h4>
+            <ul>
+              <li>Business Professional Communication</li>
+              <li>American History</li>
+              <li>Everyday Mathematics</li>
+              <li>English Composition I</li>
+              <li>Introduction to Computers</li>
+            </ul>
+          </Slide>
         </Col>
         <Col md={6}>
           <Image src={BMETImg1} fluid rounded />
@@ -90,31 +98,35 @@ const Programs = () => (
           <Image src={BMETImg2} fluid rounded />
         </Col>
         <Col md={{ span: 5, offset: 1 }}>
-          <h4>Core Component I</h4>
-          <ul>
-            <li>Professional Development</li>
-            <li> Medical Terminology & Anatomy</li>
-            <li>Networking Fundamentals I</li>
-            <li>Introduction to Mechanical Systems</li>
-            <li>Electronics I</li>
-            <li>Healthcare Technology Management I</li>
-          </ul>
+          <Slide right>
+            <h4>Core Component I</h4>
+            <ul>
+              <li>Professional Development</li>
+              <li> Medical Terminology & Anatomy</li>
+              <li>Networking Fundamentals I</li>
+              <li>Introduction to Mechanical Systems</li>
+              <li>Electronics I</li>
+              <li>Healthcare Technology Management I</li>
+            </ul>
+          </Slide>
         </Col>
       </Row>
       <Row className="mb-5">
         <Col md={6}>
-          <h4>Core Component II</h4>
-          <ul>
-            <li> Healthcare Technology Management II</li>
-            <li>Healthcare Technology Management III</li>
-            <li>Networking Fundamentals II</li>
-            <li>Biomedical Equipment I</li>
-            <li>Biomedical Equipment II</li>
-            <li>Electronics II</li>
-            <li>Advanced Medical Equipment Systems</li>
-            <li>Biomedical Database Fundamentals</li>
-            <li>Externship</li>
-          </ul>
+          <Slide left>
+            <h4>Core Component II</h4>
+            <ul>
+              <li> Healthcare Technology Management II</li>
+              <li>Healthcare Technology Management III</li>
+              <li>Networking Fundamentals II</li>
+              <li>Biomedical Equipment I</li>
+              <li>Biomedical Equipment II</li>
+              <li>Electronics II</li>
+              <li>Advanced Medical Equipment Systems</li>
+              <li>Biomedical Database Fundamentals</li>
+              <li>Externship</li>
+            </ul>
+          </Slide>
         </Col>
         <Col md={6}>
           <Image src={BMETImg4} fluid rounded />
