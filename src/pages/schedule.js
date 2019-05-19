9c5d66f72a41d544b246914c9e992e3019529d8c
Paul Castillo <azrael7@gmail.com>
2019-05-19 12:38:28 -0500

    Added utility file.

--- src/pages/schedule.js
+++ src/pages/schedule.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { getProfile } from "../utils/auth"
+import { isEmpty } from "../utils/utility"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -12,6 +13,7 @@ const Schedule = () => {
   return (
     <Layout>
       <SEO title="Schedule" />
+      {isEmpty(user) === false ? <div>User is logged in</div> : null}
       <h1>Hi from the Schedule Page</h1>
       <p>Welcome to Schedule</p>
       <Link to="/">Go back to the homepage</Link>
