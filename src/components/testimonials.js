1f1adc7501cbdbfb3db77c645edd48e5ddb5cac0
Frank Pigeon <frank.pigeonjr@gmail.com>
2019-07-24 16:03:04 -0500

    starts testimonial section for homepage.

--- src/components/testimonials.js
+++ src/components/testimonials.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { Container, Row, Col, CardGroup, Card } from "react-bootstrap"
+import { Container, Row, Col, CardGroup, Card, Button } from "react-bootstrap"
 import styled from "styled-components"
 import Zoom from "react-reveal/Zoom"
 import Jeff from "../images/testimonials/jeff.jpg"
@@ -7,11 +7,14 @@ import Jerry from "../images/testimonials/jerry.jpg"
 import Jill from "../images/testimonials/jill.jpg"
 import Josh from "../images/testimonials/josh.jpg"
 import Julia from "../images/testimonials/julia.jpg"
+import { TestimonialsData } from "../utils/utility"
 
 const TestimonialsSection = styled.section`
   padding: 96px 0;
   background-color: #f8f9fa;
 `
+
+console.table("testimonials", TestimonialsData)
 
 const Testimonials = () => (
   <TestimonialsSection>
@@ -22,61 +25,25 @@ const Testimonials = () => (
     </Container>
     <Zoom bottom>
       <CardGroup>
-        <Card>
-          <Card.Img variant="top" src={Jill} />
-          <Card.Body>
-            <Card.Title>Awesome</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={Jeff} />
-          <Card.Body>
-            <Card.Title>Graduated</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={Jerry} />
-          <Card.Body>
-            <Card.Title>Success</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={Josh} />
-          <Card.Body>
-            <Card.Title>Future is Bright</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Img variant="top" src={Julia} />
-          <Card.Body>
-            <Card.Title>Career Move</Card.Title>
-            <Card.Text>
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
+        {TestimonialsData.map(testimonial => (
+          <Card>
+            <Card.Img variant="top" src={Jill} />
+            <Card.Body>
+              <Card.Title>{testimonial.name}</Card.Title>
+              <Card.Text>
+                <p className="mt-0 mb-0">{testimonial.type}</p>
+                <p className="mt-0 mb-0">{testimonial.date}</p>
+                <p className="mt-0 mb-3">{testimonial.state}</p>
+                <p style={{ minHeight: "190px" }}>
+                  {testimonial.comment.substring(0, 240)}...
+                </p>
+                <a href="#">
+                  <Button>Learn More</Button>
+                </a>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
       </CardGroup>
     </Zoom>
   </TestimonialsSection>
