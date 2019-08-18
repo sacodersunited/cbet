b149f3b397a5ad7696d0d5575c89fb126d933795
Paul Castillo <azrael7@gmail.com>
2019-08-17 19:18:31 -0500

    Fixed txhighered png image.

--- src/components/AccreditationsStaticQuery.js
+++ src/components/AccreditationsStaticQuery.js
@@ -13,16 +13,17 @@ const AccreditationsStaticQuery = props => (
             }
           }
         }
-        txhighered: file(relativePath: { eq: "TXHigherEd.png" }) {
-          childImageSharp {
-            fluid(maxWidth: 242, quality: 100) {
-              ...GatsbyImageSharpFluid
-            }
-          }
-        }
       }
     `}
     render={data => <Accreditations images={data} />}
   />
 )
 export default AccreditationsStaticQuery
+
+// txhighered: file(relativePath: { eq: "TXHigherEd.png" }) {
+//   childImageSharp {
+//     fluid(maxWidth: 242, quality: 100) {
+//       ...GatsbyImageSharpFluid
+//     }
+//   }
+// }
