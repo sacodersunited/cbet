45f09597d9ca8aa1429489c0a212b330679ff34f
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-08-21 09:59:24 -0500

    adds quotes around testimonials

--- src/utils/utility.js
+++ src/utils/utility.js
@@ -57,40 +57,35 @@ export const TestimonialsData = [
     type: "BMET Certificate",
     state: "Texas",
     date: "May 2019",
-    comment:
-      "I have had a couple of struggles that led me to the course and keep me anchored. The course, the instructors and definitely, Mr. Gonzales and Mr. Bassuk, have been strong supporters, motivators, and just plain helpful. CBET is not your traditional tech school. The staff really cares about each and every person's success and strives with them to attain it. Thankful and Grateful.",
+    comment: `"I have had a couple of struggles that led me to the course and keep me anchored. The course, the instructors and definitely, Mr. Gonzales and Mr. Bassuk, have been strong supporters, motivators, and just plain helpful. CBET is not your traditional tech school. The staff really cares about each and every person's success and strives with them to attain it. Thankful and Grateful."`,
   },
   {
     name: "Ann R.",
     type: "BMET Degree",
     state: "Pennsylvania",
     date: "April 2019 ",
-    comment:
-      "I just started at CBET in January, so far it has been exactly what I was looking for.   The classes are informative and meaningful and the instructors are eager to help you succeed.",
+    comment: `"I just started at CBET in January, so far it has been exactly what I was looking for. The classes are informative and meaningful and the instructors are eager to help you succeed."`,
   },
   {
     name: "Kyle B.",
     type: "BMET Degree",
     state: "Minnesota",
     date: "April 2019 ",
-    comment:
-      "Made my move for my final and last career change. It is very rare to find an online accredited biomed college recognized by the industry. They have been nothing but upfront, great communication, email’s almost replied immediately and more than willing to work with my situation financially and my busy schedule since I will be working full time.",
+    comment: `"Made my move for my final and last career change. It is very rare to find an online accredited biomed college recognized by the industry. They have been nothing but upfront, great communication, email’s almost replied immediately and more than willing to work with my situation financially and my busy schedule since I will be working full time."`,
   },
   {
     name: "Joseph B.",
     type: "BMET Degree",
     state: "Georgia",
     date: "March 2019 ",
-    comment:
-      "This school was exactly what I needed coming out of the military, the classes are great, the instructors are fantastic, overall one of the best school experiences I've had. Thank you CBET for helping to get me back into the civilian workforce.",
+    comment: `"This school was exactly what I needed coming out of the military, the classes are great, the instructors are fantastic, overall one of the best school experiences I've had. Thank you CBET for helping to get me back into the civilian workforce."`,
   },
   {
     name: "Curtis F.",
     type: "BMET Certificate",
     state: "Georgia",
     date: "March 2019 ",
-    comment:
-      "The most amazing thing about the CBET culture is how welcoming, both the faculty and fellow students are to new students. There is much more than excellent academics being taught here. The program far exceeded my expectations and has gone above and beyond in helping me to increase my understanding of the Healthcare Technology Management industry, as well as unparalleled tenacity in employment assistance to get you employment in the field. The instructors are great-well informed, experienced, personable, with a teaching style that not only passes along a great deal of information in relatively short period of time, but did so with a sense of humor and perspective which made everyone in the class – regardless of their skill level, feel at ease and looking forward to the next session. Lastly, in my opinion an outstanding vocational experience in a field that is only going up!",
+    comment: `"The most amazing thing about the CBET culture is how welcoming, both the faculty and fellow students are to new students. There is much more than excellent academics being taught here. The program far exceeded my expectations and has gone above and beyond in helping me to increase my understanding of the Healthcare Technology Management industry, as well as unparalleled tenacity in employment assistance to get you employment in the field. The instructors are great-well informed, experienced, personable, with a teaching style that not only passes along a great deal of information in relatively short period of time, but did so with a sense of humor and perspective which made everyone in the class – regardless of their skill level, feel at ease and looking forward to the next session. Lastly, in my opinion an outstanding vocational experience in a field that is only going up!"`,
   },
 ]
 
