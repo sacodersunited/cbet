35f86ca556d2cf1dcb7e6e10f79e81c5b7516bfd
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2019-10-02 21:58:07 -0500

    attemps to fix icons

--- src/components/PartnersStaticQuery.js
+++ src/components/PartnersStaticQuery.js
@@ -8,64 +8,64 @@ const PartnersStaticQuery = props => (
       {
         technation: file(relativePath: { eq: "TechNation_logo.png" }) {
           childImageSharp {
-            fluid(maxHeight: 94, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         tfclogo: file(relativePath: { eq: "tfc_logo.png" }) {
           childImageSharp {
-            fluid(maxHeight: 51, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         stephens: file(relativePath: { eq: "si-recruiting.png" }) {
           childImageSharp {
-            fluid(maxHeight: 96, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         cerlogo: file(relativePath: { eq: "cer-logo.png" }) {
           childImageSharp {
-            fluid(maxHeight: 138, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         nbchambers: file(relativePath: { eq: "NBChambers-logo.png" }) {
           childImageSharp {
-            fluid(maxHeight: 142, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         chihealth: file(relativePath: { eq: "chi-health.jpg" }) {
           childImageSharp {
-            fluid(maxWidth: 400, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         citizens: file(relativePath: { eq: "citizens.png" }) {
           childImageSharp {
-            fluid(maxWidth: 400, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         miracosta: file(relativePath: { eq: "mira-costa-college.png" }) {
           childImageSharp {
-            fluid(maxWidth: 400, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
         charney: file(relativePath: { eq: "charney-logo.png" }) {
           childImageSharp {
-            fluid(maxWidth: 400, quality: 100) {
-              ...GatsbyImageSharpFluid
+            fluid(maxHeight: 600, quality: 100) {
+              ...GatsbyImageSharpFluid_withWebp
             }
           }
         }
