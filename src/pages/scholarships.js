e93db3e79be01487f031789c1aa2fa2a8c19f69d
Paul Castillo <azrael7@gmail.com>
2020-01-06 19:36:30 -0600

    Removed browser-monads package. Added links to both pdfs in ./scholarships.

--- src/pages/scholarships.js
+++ src/pages/scholarships.js
@@ -1,18 +1,16 @@
 import React from "react"
-import { document } from "browser-monads"
 import hollisMerit from "../documents/Hollis Merit Scholarship_AAS and CERT.pdf"
+import bassukScholarship from "../documents/First Sergeant Warren Bassuk Memorial Scholarship_AAS and CERT.pdf"
 
 // https://medium.com/@Jense5_/use-document-and-window-with-gatsby-e9a92ee31f36
 const Scholarships = () => {
-  let a = document.createElement("a")
-  a.href = hollisMerit
-  a.download = "scholarships"
-  a.click()
-
   return (
     <React.Fragment>
       <a href={hollisMerit} download>
         Download Hollis Merit Scholarship
+      </a>
+      <a href={bassukScholarship} download>
+        Download First Sergeant Warren Bassuk Memorial Scholarship_AAS and CERT
       </a>
     </React.Fragment>
   )
