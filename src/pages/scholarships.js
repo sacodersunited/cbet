9206c9514af6a6dbc3d2119e16353768e007f215
Paul Castillo <azrael7@gmail.com>
2020-01-06 19:23:23 -0600

    Added browser-monads for use of document. Added Scholarships page for auto-download of a pdf file.

--- src/pages/scholarships.js
+++ src/pages/scholarships.js
@@ -1,17 +1,18 @@
 import React from "react"
-import docScholarships from "../documents/CBET_Insert_BG_20.pdf"
+import { document } from "browser-monads"
+import hollisMerit from "../documents/Hollis Merit Scholarship_AAS and CERT.pdf"
 
+// https://medium.com/@Jense5_/use-document-and-window-with-gatsby-e9a92ee31f36
 const Scholarships = () => {
   let a = document.createElement("a")
-  //   console.log("doc schol", docScholarships)
-  a.href = docScholarships
+  a.href = hollisMerit
   a.download = "scholarships"
   a.click()
 
   return (
     <React.Fragment>
-      <a href={docScholarships} download>
-        download Scholarships
+      <a href={hollisMerit} download>
+        Download Hollis Merit Scholarship
       </a>
     </React.Fragment>
   )
