6f27b6999912b81d3ad3639b5ae40af7a084f69d
Paul Castillo <azrael7@gmail.com>
2020-01-25 23:19:15 -0600

    Added visual testing for the homepage only.

--- cypress/e2e/cbet_nav.test.js
+++ cypress/e2e/cbet_nav.test.js
@@ -1,21 +1,42 @@
+/// <reference types="cypress" />
+/// <reference types="@applitools/eyes-cypress" />
+
 describe("cbet test nav", () => {
+  beforeEach(() => {
+    cy.eyesOpen({
+      appName: "CBET",
+      testName: "1920 by 1080 on top 3 browsers",
+      browser: [
+        { width: 1920, height: 1080, name: "safari" },
+        { width: 1920, height: 1080, name: "chrome" },
+        { width: 1920, height: 1080, name: "ie11" },
+      ],
+    })
+  })
+
+  afterEach(() => {
+    cy.eyesClose()
+  })
+
   it("works", () => {
     cy.viewport(1920, 1080)
     cy.visit("/")
 
+    cy.eyesCheckWindow()
     cy.get('.mr-auto > [href="/about"]').click({ force: true })
     cy.url().should("include", "/about")
 
-    cy.get(".dropdown-toggle")
-      .click({ force: true })
-      .get('[href="/bmet-certificate"]')
-      .click({ force: true })
+    cy.get(".dropdown-toggle").click({ force: true })
+    cy.get('[href="/bmet-certificate"]').click({ force: true })
     cy.url().should("include", "/bmet-certificate")
 
     cy.get(".dropdown-toggle").click({ force: true })
+    cy.get(".dropdown-toggle").click({ force: true })
+
     cy.get('[href="/bmet-degree"]').click({ force: true })
     cy.url().should("include", "/bmet-degree")
 
+    cy.get(".dropdown-toggle").click({ force: true })
     cy.get(".dropdown-toggle").click({ force: true })
     cy.get('[href="/it-certificate"]').click({ force: true })
     cy.url().should("include", "/it-certificate")
