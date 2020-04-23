32be8f1f5b063d847624f36a4cccd64f74b58a6b
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-04-22 21:14:40 -0500

    adds more content for blog post
    adds no carousel option for careers layout

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,22 +1,54 @@
-import React, { useEffect, useState } from "react"
+import React from "react"
 // import Slide from "react-reveal/Slide"
 import Layout from "../components/careers/layout"
-import { Link } from "gatsby"
-import SEO from "../components/seo"
-import useJobs from "../hooks/use-jobs"
-import useJobsBG from "../hooks/use-jobs-bg"
-import useEvents from "../hooks/use-events"
-import useCbetAuth from "../hooks/use-cbet-auth"
-import JobsCarousel from "../components/careers/JobsCarousel.js"
-import JobDetail from "../components/careers/JobDetail"
-import { Container, Col, Row, Button, Image, Badge } from "react-bootstrap"
+import { FaClock } from "react-icons/fa"
+import { Image, Badge, ResponsiveEmbed } from "react-bootstrap"
 
 export default function Blog() {
   return (
-    <Layout>
-      <h1>AWESOME BLOG TITLE</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Exercitationem, at sequi totam ad fugiat dolore non necessitatibus eveniet ipsa illo minima corrupti modi vero! Unde facilis est pariatur quis? Iure!</p>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta consequuntur perspiciatis, vero adipisci, laboriosam dolores omnis possimus est officia doloribus quia? Veritatis dolores sapiente perferendis a dolorem autem porro.s</p>
-    </Layout>
+    <>
+      <Layout noCarousel>
+        <h1>Lessons From the Trenches</h1>
+        <section className="post-meta">
+          <p>
+            by Scott Hanselman posted on: 4/22/2020 <FaClock /> 1h ago
+          </p>
+        </section>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Exercitationem, at sequi totam ad fugiat dolore non necessitatibus
+          eveniet ipsa illo minima corrupti modi vero! Unde facilis est pariatur
+          quis? Iure!
+        </p>
+        <Image
+          src="https://i.picsum.photos/id/155/500/300.jpg"
+          alt="train"
+          fluid
+        />
+        <h2 className="mt-3">Officia Doloribus</h2>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim dicta
+          consequuntur perspiciatis, vero adipisci, laboriosam dolores omnis
+          possimus est officia doloribus quia? Veritatis dolores sapiente
+          perferendis a dolorem autem porro.s
+        </p>
+        <h2>Vero Adipisci</h2>
+        <ul>
+          <li>Lorem ipsum dolor sit</li>
+          <li>Laborum ipsam quasi laudantium qui</li>
+          <li>Suscipit nesciunt optio dignissimos</li>
+        </ul>
+        <ResponsiveEmbed aspectRatio="16by9">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/zpOULjyy-n8"
+            frameborder="0"
+            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </ResponsiveEmbed>
+      </Layout>
+    </>
   )
 }
