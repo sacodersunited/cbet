0cd3a8f549c8b653c9ab5e8cdb905d3376aef50d
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-05-01 00:05:19 -0500

    last commit for tonight

--- src/components/admin/layout.js
+++ src/components/admin/layout.js
@@ -5,27 +5,48 @@ import { Link } from "gatsby"
 export default function Layout({ children }) {
   return (
     <Container fluid>
-      <Row>
+      <Row className="flex-xl-nowrap">
         <Col
           md={2}
           style={{
             height: "100vh",
             color: "whitesmoke",
             backgroundColor: "#43425d",
+            display: "block !important",
+            overflow: "hidden",
+            position: "sticky",
+            top: "4 rem",
+            zIndex: "1000",
           }}
         >
           <h4 className="mt-5 mb-3">CBET Admin</h4>
-          <Nav defaultActiveKey="/admin" className="flex-column">
-            <Link to="/admin" className="nav-link" activeClassName="active">
+          <Nav defaultActiveKey="/admin" className="flex-column" fill justify>
+            <Link
+              to="/admin"
+              className="nav-link text-light"
+              activeClassName="active"
+            >
               Dashboard
             </Link>
-            <Link to="/admin" className="nav-link" activeClassName="active">
+            <Link
+              to="/admin"
+              className="nav-link text-light"
+              activeClassName="active"
+            >
               Jobs
             </Link>
-            <Link to="/admin" className="nav-link" activeClassName="active">
+            <Link
+              to="/admin"
+              className="nav-link text-light"
+              activeClassName="active"
+            >
               Events
             </Link>
-            <Link to="/admin" className="nav-link" activeClassName="active">
+            <Link
+              to="/admin"
+              className="nav-link text-light"
+              activeClassName="active"
+            >
               Blog
             </Link>
           </Nav>
