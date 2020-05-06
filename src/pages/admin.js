a1079bd18988a91bb0cbb1bd68d6fc30ea6f5870
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-05-05 22:27:21 -0500

    adds HOC in order to pass cbetData to admin modules

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -5,7 +5,7 @@ import Layout from "../components/admin/layout"
 const Admin = () => {
   return (
     <Layout title="Dashboard">
-      <SEO title="Admin" />
+      <SEO title="Admin Dashboard" />
       <p>edit the this page from </p>
       <pre className="p-3 mb-2 bg-dark text-white">/src/pages/admin</pre>
     </Layout>
