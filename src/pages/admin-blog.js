d17bf45675b9a045731cc49d63d2d294ee7e2900
Paul <azrael7@gmail.com>
2020-05-06 08:47:53 -0500

    Refactored Layout to original form. Added cbetcontent to admin-blog.

--- src/pages/admin-blog.js
+++ src/pages/admin-blog.js
@@ -1,19 +1,38 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import SEO from "../components/seo"
 import Layout from "../components/admin/layout"
+import useCbetAuth from "../hooks/use-cbet-auth"
 
-const AdminBlog = props => {
-  const user = null
-  console.log("user", user)
+export default function AdminBlog() {
+  const authContent = useCbetAuth()
+  const [cbetContent, setCbetContent] = useState([])
+
+  useEffect(() => {
+    fetch(
+      `https://cbetcontent.azurewebsites.net/api/GetCbetContent?code=${authContent}`
+    )
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        setCbetContent(resultData)
+      })
+  }, [authContent])
+
+  if (cbetContent.length > 0) {
+    console.log(`cbetContent ${cbetContent}`)
+    console.log(
+      "test",
+      cbetContent.map(cat => {
+        console.log("cat", cat)
+        return cat
+      })
+    )
+  }
 
   return (
-    <Layout title="Admin Blog" >
-      
+    <Layout title="Admin Blog">
       <SEO title="Admin" />
       <p>edit the this page from </p>
       <pre className="p-3 mb-2 bg-dark text-white">/src/pages/admin-blog</pre>
     </Layout>
   )
 }
-
-export default AdminBlog
