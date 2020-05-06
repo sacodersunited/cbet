4a385ab3cb7fcf47a92faf1e0048afea1a30f24e
Paul <azrael7@gmail.com>
2020-05-06 10:22:10 -0500

    Added newest version of react-bootstrap. Added Validation to controls.

--- src/pages/admin-create.js
+++ src/pages/admin-create.js
@@ -9,91 +9,107 @@ import CbetDropzone from "../components/CbetDropzone"
 
 const AdminCreate = () => {
   const { register, handleSubmit, watch, errors } = useForm()
+
+  const onSubmit = data => console.log(data)
+
   return (
     <Layout title="Create/Edit">
       <SEO title="Admin Create Edit" />
       <Container fluid>
         <Row>
           <Col md={4}>
-            <Form.Group controlId="selectCategory">
-              <Form.Control as="select" name="category" ref={register()}>
-                <option value="">Select</option>
-                <option value="Job">Job</option>
-                <option value="Event">Event</option>
-                <option value="Blog">Blog</option>
-              </Form.Control>
-              <Form.Label>Job, Event or Blog Post</Form.Label>
-            </Form.Group>
-
-            <Form.Group controlId="TitleHere">
-              <Form.Control type="text" ref={register()}></Form.Control>
-              <Form.Label>Title of Blog Post</Form.Label>
-            </Form.Group>
-
-            <Form.Group controlId="AuthorHere">
-              <Form.Control type="text" ref={register()}></Form.Control>
-              <Form.Label>Author</Form.Label>
-            </Form.Group>
-
-            <Form.Row controlId="selectCategory">
-              <Form.Group as={Col}>
+            <Form onSubmit={handleSubmit(onSubmit)}>
+              <Form.Group controlId="selectCategory">
                 <Form.Control
-                  style={{ width: "75px" }}
                   as="select"
-                  name="month"
-                  ref={register()}
+                  name="category"
+                  ref={register({ required: true })}
                 >
-                  <option value="0">0</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
+                  <option value="">Select</option>
+                  <option value="Job">Job</option>
+                  <option value="Event">Event</option>
+                  <option value="Blog">Blog</option>
                 </Form.Control>
-                <Form.Label>Month</Form.Label>
+                <Form.Label>Job, Event or Blog Post</Form.Label>
+                {errors.catgegory && "Cbet Category is required"}
               </Form.Group>
 
-              <Form.Group as={Col}>
+              <Form.Group controlId="TitleHere">
                 <Form.Control
-                  style={{ width: "75px" }}
-                  as="select"
-                  name="day"
-                  ref={register()}
-                >
-                  <option value="0">0</option>
-                  <option value="1">1</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </Form.Control>
-                <Form.Label>Day</Form.Label>
+                  type="text"
+                  ref={register({ required: true })}
+                ></Form.Control>
+                <Form.Label>Title of Blog Post</Form.Label>
               </Form.Group>
 
-              <Form.Group as={Col}>
+              <Form.Group controlId="AuthorHere">
                 <Form.Control
-                  style={{ width: "100px" }}
-                  as="select"
-                  name="year"
-                  ref={register()}
-                >
-                  <option value="2020">2020</option>
-                  <option value="2019">2019</option>
-                  <option value="2">2</option>
-                  <option value="3">3</option>
-                </Form.Control>
-                <Form.Label>Year</Form.Label>
+                  type="text"
+                  ref={register({ required: true })}
+                ></Form.Control>
+                <Form.Label>Author</Form.Label>
               </Form.Group>
-            </Form.Row>
 
-            <Form.Group>
-              <CbetDropzone></CbetDropzone>
-            </Form.Group>
+              <Form.Row controlId="selectCategory">
+                <Form.Group as={Col}>
+                  <Form.Control
+                    style={{ width: "75px" }}
+                    as="select"
+                    name="month"
+                    ref={register({ required: true })}
+                  >
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Control>
+                  <Form.Label>Month</Form.Label>
+                </Form.Group>
 
-            <Form.Row>
-              <Form.Group as={Col}>
-                <Button>Save</Button>
+                <Form.Group as={Col}>
+                  <Form.Control
+                    style={{ width: "75px" }}
+                    as="select"
+                    name="day"
+                    ref={register({ required: true })}
+                  >
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Control>
+                  <Form.Label>Day</Form.Label>
+                </Form.Group>
+
+                <Form.Group as={Col}>
+                  <Form.Control
+                    style={{ width: "100px" }}
+                    as="select"
+                    name="year"
+                    ref={register({ required: true })}
+                  >
+                    <option value="2020">2020</option>
+                    <option value="2019">2019</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                  </Form.Control>
+                  <Form.Label>Year</Form.Label>
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group>
+                <CbetDropzone></CbetDropzone>
               </Form.Group>
-              <Form.Group as={Col}>
-                <Button>Cancel</Button>
-              </Form.Group>
-            </Form.Row>
+
+              <Form.Row>
+                <Form.Group as={Col}>
+                  <Button>Save</Button>
+                </Form.Group>
+                <Form.Group as={Col}>
+                  <Button>Cancel</Button>
+                </Form.Group>
+              </Form.Row>
+            </Form>
           </Col>
           <Col md={8}>
             <SunEditor />
