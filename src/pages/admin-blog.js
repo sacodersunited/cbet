0161c745e06dfca0db4c34c2360ceba2a1b2ae94
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-05-19 14:45:42 -0500

    adds active state

--- src/pages/admin-blog.js
+++ src/pages/admin-blog.js
@@ -4,7 +4,7 @@ import Layout from "../components/admin/layout"
 import useCbetAuth from "../hooks/use-cbet-auth"
 import { Container, Row, Col, Card, Button, Badge } from "react-bootstrap"
 import Moment from "react-moment"
-import { FaPen, FaTimes } from "react-icons/fa"
+import { FaPen, FaTimes, FaCircle } from "react-icons/fa"
 
 export default function AdminBlog() {
   const authContent = useCbetAuth()
@@ -45,6 +45,27 @@ export default function AdminBlog() {
                     </Badge>
                   </Card.Title>
                   <Card.Text>
+                    <div className="meta">
+                      {post.Status ? (
+                        <small>
+                          <FaCircle
+                            className="mr-1"
+                            color="#5EE2A0"
+                            style={{ verticalAlign: "baseline" }}
+                          />
+                          Active
+                        </small>
+                      ) : (
+                        <small>
+                          <FaCircle
+                            className="mr-1"
+                            color="#E25E5E"
+                            style={{ verticalAlign: "baseline" }}
+                          />
+                          Disabled
+                        </small>
+                      )}
+                    </div>
                     {/* copied from stack overflow, do not trust */}
                     {/* https://stackoverflow.com/questions/55418929/how-can-i-remove-html-markup-from-strings-in-state-data/55419024 */}
                     {post.Description.slice(0, 140).replace(
