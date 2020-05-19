92fb43bd3ab0cd57e926b78832d9ff055de5bf6f
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-05-19 17:07:01 -0500

    adds read data for all admin areas - except dashboard - adds helpers for reused comps - more styles

--- src/pages/admin-events.js
+++ src/pages/admin-events.js
@@ -2,10 +2,17 @@ import React, { useState, useEffect } from "react"
 import SEO from "../components/seo"
 import Layout from "../components/admin/layout"
 import useCbetAuth from "../hooks/use-cbet-auth"
-import { Container, Row, Col, Card, Badge } from "react-bootstrap"
+import { Container, Row, Col, Card, Badge, Button } from "react-bootstrap"
 import Moment from "react-moment"
 import { FaPen, FaTimes } from "react-icons/fa"
-import { showActive } from "../utils/admin"
+import { showActive, showDate, showAddress } from "../utils/admin"
+import styled from "styled-components"
+
+const MetaSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 10px;
+`
 
 export default function AdminEvents() {
   const authContent = useCbetAuth()
@@ -34,8 +41,8 @@ export default function AdminEvents() {
                 <Card.Header className="d-flex justify-content-between">
                   {event.CategoryName}
                   <div className="meta-edit">
-                    <FaPen />
-                    <FaTimes className="ml-2" />
+                    <FaPen color="#A4AFB7" />
+                    <FaTimes color="#A4AFB7" className="ml-2" />
                   </div>
                 </Card.Header>
                 <Card.Body style={{ minHeight: "200px" }}>
@@ -46,7 +53,17 @@ export default function AdminEvents() {
                     </Badge>
                   </Card.Title>
                   <Card.Text>
-                    <div className="meta">{showActive(event.Status)}</div>
+                    <MetaSection>
+                      {showActive(event.Status)}
+                      {showDate(
+                        "Modified Date",
+                        event.ModifyDate,
+                        event.CreatedDate
+                      )}
+                      {showDate("Start Date", event.StartDate)}
+                      {showDate("End Date", event.EndDate)}
+                      {showAddress(event.Location)}
+                    </MetaSection>
                     {/* copied from stack overflow, do not trust */}
                     {/* https://stackoverflow.com/questions/55418929/how-can-i-remove-html-markup-from-strings-in-state-data/55419024 */}
                     {event.Description.slice(0, 140).replace(
@@ -54,7 +71,13 @@ export default function AdminEvents() {
                       ""
                     ) + "..."}
                   </Card.Text>
-                  {/* <Button variant="primary">Go somewhere</Button> */}
+                  <a
+                    href={event.Link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <Button variant="outline-primary">Event Link</Button>
+                  </a>
                 </Card.Body>
               </Card>
             </Col>
--- src/utils/admin.js
+++ src/utils/admin.js
@@ -1,6 +1,8 @@
 import React from "react"
-import { FaCircle } from "react-icons/fa"
+import { FaCircle, FaCalendar, FaPen, FaGlobe } from "react-icons/fa"
+import Moment from "react-moment"
 
+// cat is short for category
 export const showActive = catIsActive => {
   return catIsActive ? (
     <small>
@@ -19,6 +21,46 @@ export const showActive = catIsActive => {
         style={{ verticalAlign: "baseline" }}
       />
       Disabled
+    </small>
+  )
+}
+
+export const showDate = (title, catDate, createDate) => {
+  return (
+    <small>
+      <FaCalendar
+        className="mr-1"
+        color="#A5A4BF"
+        style={{ verticalAlign: "baseline" }}
+      />
+      {title}:{" "}
+      <Moment format="MM/DD/YYYY">{catDate ? catDate : createDate}</Moment>
+    </small>
+  )
+}
+
+export const showAuthor = author => {
+  return (
+    <small>
+      <FaPen
+        className="mr-1"
+        color="#A5A4BF"
+        style={{ verticalAlign: "baseline" }}
+      />
+      Author: {author}
+    </small>
+  )
+}
+
+export const showAddress = address => {
+  return (
+    <small>
+      <FaGlobe
+        className="mr-1"
+        color="#A5A4BF"
+        style={{ verticalAlign: "baseline" }}
+      />
+      {address}
     </small>
   )
 }
