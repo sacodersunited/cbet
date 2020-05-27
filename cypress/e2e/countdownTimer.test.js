89dfc0599196a5cdb1cb76b782a63b65cb615a0e
Paul <azrael7@gmail.com>
2020-05-26 22:50:10 -0500

    Adds a way to log in to MS Azure.

--- cypress/e2e/countdownTimer.test.js
+++ cypress/e2e/countdownTimer.test.js
@@ -1,86 +1,18 @@
 /// <reference types="cypress" />
-import { AuthenticationContext } from "react-adal"
+// import { AuthenticationContext } from "react-adal"
 // import { azTenantId, azClientId } from "../../server/config/environment"
-
-const adalConfig = {
-  tenant: "cbettenant.onmicrosoft.com",
-  clientId: "f52faa18-493f-44c7-9b2e-56a41f489147",
-  cacheLocation: "localStorage",
-  replyUrl: "/",
-  endpoints: {
-    api: "",
-  },
-}
-const authContext = new AuthenticationContext(adalConfig)
-
-export default async function doLogin() {
-  // getCachedToken also does an expiration check so we know for sure the tokens are usable
-  if (
-    !authContext.getCachedToken(adalConfig.endpoints.api) ||
-    !authContext.getCachedToken(adalConfig.clientId)
-  ) {
-    const response = await cy.request({
-      method: "POST",
-      url: "https://cbettenant.onmicrosoft.com.b2clogin.com/tfp/",
-      // qs: { 'api-version': '1.0' }, // uncomment if your consuming resource expects the 'aud' to have a prefix of 'sn:'
-      headers: {
-        "cache-control": "no-cache",
-        "content-type":
-          "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
-      },
-      form: true,
-      body: {
-        grant_type: "password",
-        response_type: "code",
-        // client_secret: "[[yourappsclientsecret]]",
-        client_id: "f52faa18-493f-44c7-9b2e-56a41f489147",
-        username: "376888@park.edu",
-        password: "Analysis",
-        scope: [
-          "https://graph.microsoft.com/Directory.Read.All",
-          "https://cbettenant.onmicrosoft.com/api-1/user_impersonation",
-        ],
-        // resource: "[[some-resource-id]]",
-      },
-    })
-    // Store the token and data in the location where adal expects it
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.IDTOKEN,
-      response.body.access_token
-    )
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.ACCESS_TOKEN_KEY + adalConfig.endpoints.api,
-      response.body.access_token
-    )
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.ACCESS_TOKEN_KEY + adalConfig.clientId,
-      response.body.access_token
-    )
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.EXPIRATION_KEY + adalConfig.endpoints.api,
-      response.body.expires_on
-    )
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.EXPIRATION_KEY + adalConfig.clientId,
-      response.body.expires_on
-    )
-    authContext._saveItem(
-      authContext.CONSTANTS.STORAGE.TOKEN_KEYS,
-      [adalConfig.clientId].join(authContext.CONSTANTS.RESOURCE_DELIMETER) +
-        authContext.CONSTANTS.RESOURCE_DELIMETER
-    )
-  }
-}
 
 describe("cbet test Countdown", () => {
   it("works", () => {
     cy.viewport(1920, 1080)
     cy.visit("http://localhost:8000/")
 
-    cy.get("#form-dropdown").click({ force: true })
-    cy.get("form").should("exist")
+    cy.login()
+    // cy.visit("https://login.microsoftonline.com")
+    cy.visit("/admin")
 
-    doLogin()
+    cy.get(":nth-child(2) > .btn").click({ force: true })
+    // doLogin()
     // cy.request("http://dev.local/seed")
     // cy.get(".d-inline-block > picture > img").click({ force: true })
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -26,3 +26,33 @@
 import "@testing-library/cypress/add-commands"
 import "./commands"
 import "cypress-axe"
+
+Cypress.Commands.add("login", () => {
+  cy.request({
+    method: "POST",
+    url: `https://login.microsoftonline.com/${Cypress.config(
+      "tenantId"
+    )}/oauth2/token`,
+    form: true,
+    body: {
+      grant_type: "client_credentials",
+      client_id: Cypress.config("clientId"),
+      client_secret: Cypress.config("clientSecret"),
+      resource: Cypress.config("clientId"),
+    },
+  }).then(response => {
+    const ADALToken = response.body.access_token
+    const expiresOn = response.body.expires_on
+
+    localStorage.setItem("adal.token.keys", `${Cypress.config("clientId")}|`)
+    localStorage.setItem(
+      `adal.access.token.key${Cypress.config("clientId")}`,
+      ADALToken
+    )
+    localStorage.setItem(
+      `adal.expiration.key${Cypress.config("clientId")}`,
+      expiresOn
+    )
+    localStorage.setItem("adal.idtoken", ADALToken)
+  })
+})
