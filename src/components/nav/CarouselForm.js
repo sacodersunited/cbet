ec9740eeaf29bfda3583b67e37ef3129cb05feb1
Paul <azrael7@gmail.com>
2020-05-27 17:42:18 -0500

    Fixing nav test.

--- src/components/nav/CarouselForm.js
+++ src/components/nav/CarouselForm.js
@@ -134,6 +134,7 @@ class CarouselForm extends React.Component {
         validated={this.state.validated}
         onSubmit={e => this.handleSubmit(e)}
         style={{ padding: "10px 40px", minWidth: "400px" }}
+        data-cy="EnrollmentForm"
       >
         <p className="lead">
           Find out how CBET can help you succeed with your future
