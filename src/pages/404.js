ebaad8f724fd6c042615cf6dc464e5437a7ede36
Frank Pigeon <frank.pigeonjr@gmail.com>
2020-06-04 15:37:13 -0500

    Inquiry Form
    Adds an example of the form on the 404 page.
    Uses react useEffect

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,31 @@
-import React from "react"
-
+import React, { useEffect } from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Container } from "react-bootstrap"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+export default function NotFoundPage() {
+  // copied from stackoverflow, do not trust
+  // https://stackoverflow.com/questions/34424845/adding-script-tag-to-react-jsx
+  useEffect(() => {
+    const script = document.createElement("script")
+    script.src =
+      "https://cbet.quickschools.com/sms/es/enquiry?divId=enquiry-form"
+    script.async = true
 
-export default NotFoundPage
+    document.body.appendChild(script)
+
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
+  return (
+    <Layout>
+      <Container>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <div id="enquiry-form" />
+      </Container>
+    </Layout>
+  )
+}
