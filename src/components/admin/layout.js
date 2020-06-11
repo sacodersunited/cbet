9b7106a267f07beeac6fe6831365d95ab46d72aa
Paul <azrael7@gmail.com>
2020-06-11 13:37:24 -0500

    Removed document check on layout.

--- src/components/admin/layout.js
+++ src/components/admin/layout.js
@@ -98,59 +98,59 @@ export default function Layout({ title, category, clickNew, children }) {
     }
   }
 
-  if (typeof document !== `undefined`) {
-    return (
-      <AzureAD provider={signInAuthProvider} forceLogin={true}>
-        {({ login, logout, authenticationState, error, accountInfo }) => {
-          switch (authenticationState) {
-            case AuthenticationState.Authenticated:
-              return (
-                <Container fluid>
-                  <Row className="flex-xl-nowrap">
-                    <SideNav />
-                    <Col
-                      md={10}
-                      className="pb-5"
-                      style={{ backgroundColor: "#f0f0f7" }}
-                    >
-                      <Navbar expand="lg" variant="light" bg="light">
-                        {/* <Form className="w-75" inline>
+  // if (typeof document !== `undefined`) {
+  return (
+    <AzureAD provider={signInAuthProvider} forceLogin={true}>
+      {({ login, logout, authenticationState, error, accountInfo }) => {
+        switch (authenticationState) {
+          case AuthenticationState.Authenticated:
+            return (
+              <Container fluid>
+                <Row className="flex-xl-nowrap">
+                  <SideNav />
+                  <Col
+                    md={10}
+                    className="pb-5"
+                    style={{ backgroundColor: "#f0f0f7" }}
+                  >
+                    <Navbar expand="lg" variant="light" bg="light">
+                      {/* <Form className="w-75" inline>
                     <FormControl
                       type="text"
                       placeholder="&#x1F50D; Search for jobs, events, and posts"
                       className="mr-sm-2 w-100"
                     />
                   </Form> */}
-                        <Navbar.Toggle />
-                        <Navbar.Collapse className="justify-content-end">
-                          <NavDropdown
-                            title={`Signed in as ${accountInfo.account.name}`}
-                            id="basic-nav-dropdown"
+                      <Navbar.Toggle />
+                      <Navbar.Collapse className="justify-content-end">
+                        <NavDropdown
+                          title={`Signed in as ${accountInfo.account.name}`}
+                          id="basic-nav-dropdown"
+                        >
+                          <NavDropdown.Item onClick={logout}>
+                            Logout
+                          </NavDropdown.Item>
+                        </NavDropdown>
+                      </Navbar.Collapse>
+                    </Navbar>
+                    <Container>
+                      <Row className="pt-5 pb-5">
+                        <Col md={3}>
+                          {category === undefined ? (
+                            <h2>{`${title}`}</h2>
+                          ) : (
+                            <h2>{`${title} - ${category}`}</h2>
+                          )}
+                        </Col>
+                        <Col md={2}>
+                          <Button
+                            variant="outline-primary"
+                            onClick={createNewCbetContent}
                           >
-                            <NavDropdown.Item onClick={logout}>
-                              Logout
-                            </NavDropdown.Item>
-                          </NavDropdown>
-                        </Navbar.Collapse>
-                      </Navbar>
-                      <Container>
-                        <Row className="pt-5 pb-5">
-                          <Col md={3}>
-                            {category === undefined ? (
-                              <h2>{`${title}`}</h2>
-                            ) : (
-                              <h2>{`${title} - ${category}`}</h2>
-                            )}
-                          </Col>
-                          <Col md={2}>
-                            <Button
-                              variant="outline-primary"
-                              onClick={createNewCbetContent}
-                            >
-                              Create New
-                            </Button>
-                          </Col>
-                          {/* <Col md={2}>
+                            Create New
+                          </Button>
+                        </Col>
+                        {/* <Col md={2}>
                       <p>
                         Filter One <FaChevronDown />
                       </p>
@@ -160,45 +160,44 @@ export default function Layout({ title, category, clickNew, children }) {
                         Filter Two <FaChevronDown />
                       </p>
                     </Col> */}
-                        </Row>
-                      </Container>
-                      {children}
-                    </Col>
-                  </Row>
-                </Container>
-              )
-            case AuthenticationState.Unauthenticated:
-              // TODO: Added a logout button but the AUth client kicks in....may not need this or authenticating state to the app
-              return (
-                <div>
-                  {error && (
-                    <p>
-                      <span>
-                        An error occurred during authentication, please try
-                        again!
-                      </span>
-                    </p>
-                  )}
+                      </Row>
+                    </Container>
+                    {children}
+                  </Col>
+                </Row>
+              </Container>
+            )
+          case AuthenticationState.Unauthenticated:
+            // TODO: Added a logout button but the AUth client kicks in....may not need this or authenticating state to the app
+            return (
+              <div>
+                {error && (
                   <p>
-                    <span>Please Login to continue.</span>
-                    <Button onClick={login}>Login</Button>
+                    <span>
+                      An error occurred during authentication, please try again!
+                    </span>
                   </p>
-                </div>
-              )
-            case AuthenticationState.InProgress:
-              // TODO: ADD Cbet Styles for unauthenticating
-              return (
-                <StyledLoadingBG>
-                  <p style={{ fontSize: "62px" }}>Authenticating...</p>
-                </StyledLoadingBG>
-              )
-            default:
-              return null
-          }
-        }}
-      </AzureAD>
-    )
-  } else {
-    return ""
-  }
+                )}
+                <p>
+                  <span>Please Login to continue.</span>
+                  <Button onClick={login}>Login</Button>
+                </p>
+              </div>
+            )
+          case AuthenticationState.InProgress:
+            // TODO: ADD Cbet Styles for unauthenticating
+            return (
+              <StyledLoadingBG>
+                <p style={{ fontSize: "62px" }}>Authenticating...</p>
+              </StyledLoadingBG>
+            )
+          default:
+            return null
+        }
+      }}
+    </AzureAD>
+  )
+  // } else {
+  //   return ""
+  // }
 }
