b228190833ff6282183e008c311d5f747c90a285
Paul <azrael7@gmail.com>
2021-03-15 15:31:25 -0500

    Added container on purdue page.

--- src/pages/purdue-crcst.js
+++ src/pages/purdue-crcst.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Container, Row, Col } from "react-bootstrap"
 import Layout from "../components/layout"
 import { UseScript } from "../hooks/use-script"
 
@@ -10,15 +11,21 @@ const PurdueCrcst = (props) => {
   }
   return (
     <Layout>
-      <iframe
-        id="typeform-full"
-        title="typeformfull"
-        width="100%"
-        height="100%"
-        frameborder="0"
-        allow="camera; microphone; autoplay; encrypted-media;"
-        src="https://form.typeform.com/to/zoUUeneK?typeform-medium=embed-snippet"
-      ></iframe>
+      <Container className="mb-5">
+        <Row>
+          <Col md={12}>
+            <iframe
+              id="typeform-full"
+              title="typeformfull"
+              width="100%"
+              height="100%"
+              frameborder="0"
+              allow="camera; microphone; autoplay; encrypted-media;"
+              src="https://form.typeform.com/to/zoUUeneK?typeform-medium=embed-snippet"
+            ></iframe>
+          </Col>
+        </Row>
+      </Container>
     </Layout>
   )
 }
