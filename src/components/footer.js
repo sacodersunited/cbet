391681125c562bf1fdd28395979c88f265408115
Paul <azrael@gmail.com>
2021-04-08 20:50:18 -0500

    Changed bmetcert length.

--- src/components/footer.js
+++ src/components/footer.js
@@ -51,7 +51,7 @@ const Footer = () => (
 
             <a className="d-block text-light mb-3" href="/course-catalog">
               Course Catalog
-            </Link>
+            </a>
             <a
               className="d-block text-light mb-3"
               href="mailto:registrar@cbet.edu?Subject=Transcript%20Request"
