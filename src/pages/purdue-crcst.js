71b04cf9c208e7cfdb6a9de0366bee813c268481
Paul C <azrael7@gmail.com>
2021-08-23 11:30:17 -0500

    Updated purdue page.

--- src/pages/purdue-crcst.js
+++ src/pages/purdue-crcst.js
@@ -5,15 +5,6 @@ import Layout from "../components/layout"
 const PurdueCrcst = (props) => {
   return (
     <Layout>
-      <iframe
-        id="typeform-full"
-        title="typeformfull"
-        width="100%"
-        height="100%"
-        frameborder="0"
-        allow="camera; microphone; autoplay; encrypted-media;"
-        src="https://form.typeform.com/to/zoUUeneK?typeform-medium=embed-snippet"
-      ></iframe>
       <Container className="mb-5">
         <Row>
           <Col md={12}>
