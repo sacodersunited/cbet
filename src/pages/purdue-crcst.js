fd92e62f7d87ca748d84b5686ed7838e662b2efb
Paul C <azrael7@gmail.com>
2021-11-11 17:16:23 -0600

    Tried adding some padding to alert on purdue page.

--- src/pages/purdue-crcst.js
+++ src/pages/purdue-crcst.js
@@ -5,26 +5,20 @@ import Layout from "../components/layout"
 const PurdueCrcst = (props) => {
   return (
     <Layout>
-      <Container className="mb-5">
-        <Row>
-          <Col md={12}>
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <br />
-            <Alert variant="danger" dismissible>
-              <div className="container">
-                <Alert.Heading>
-                  Special Notice Concerning the Global COVID-19 Pandemic
-                </Alert.Heading>
-                  Due to the impact of COVID-19, we have temporarily suspended our CRCST Externship Program. Please contact a Purdue Global advisor for additional information and support.
-              </div>
-            </Alert>
-          </Col>
-        </Row>
-      </Container>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Alert variant="danger" dismissible>
+        <div className="container">
+          <Alert.Heading>
+            Special Notice Concerning the Global COVID-19 Pandemic
+          </Alert.Heading>
+            Due to the impact of COVID-19, we have temporarily suspended our CRCST Externship Program. Please contact a Purdue Global advisor for additional information and support.
+        </div>
+      </Alert>
+      <br></br>
+      <br></br>
+      <br></br>
     </Layout>
   )
   }
