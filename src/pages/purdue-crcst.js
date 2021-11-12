dc23bf45ca7e050182fbc3a36d5919b658667919
Paul C <azrael7@gmail.com>
2021-11-12 14:05:38 -0600

    Changed purdue message.

--- src/pages/purdue-crcst.js
+++ src/pages/purdue-crcst.js
@@ -1,26 +1,33 @@
 import React from "react"
-import { Container, Row, Col, Alert } from "react-bootstrap"
+import { Modal } from "react-bootstrap"
 import Layout from "../components/layout"
 
 const PurdueCrcst = (props) => {
   return (
     <Layout>
-      <br></br>
-      <br></br>
-      <br></br>
-      <Alert variant="danger" dismissible>
-        <div className="container">
-          <Alert.Heading>
-            Special Notice Concerning the Global COVID-19 Pandemic
-          </Alert.Heading>
-            Due to the impact of COVID-19, we have temporarily suspended our CRCST Externship Program. Please contact a Purdue Global advisor for additional information and support.
-        </div>
-      </Alert>
-      <br></br>
-      <br></br>
-      <br></br>
+      <div style={{ height: '500px'}}>
+        <Modal.Dialog
+          size="xl"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton style={{ background: '#32679a'}}> 
+            <Modal.Title id="contained-modal-title-vcenter">
+              Purdue CRCST Message
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            {/* <h4>Centered Modal</h4> */}
+            <p>
+              Due to the impact of COVID-19, we have temporarily suspended our
+              CRCST Externship Program. Please contact a Purdue Global advisor
+              for additional information and support.
+            </p>
+          </Modal.Body>
+        </Modal.Dialog>
+      </div>
     </Layout>
   )
-  }
+}
 
-  export default PurdueCrcst
+export default PurdueCrcst
